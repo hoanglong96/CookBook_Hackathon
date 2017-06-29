44979729117df096713396d3633aa9b0f6048fa6
HOANGLONG-PC\HoangLong <long11hq@gmail.com>
2017-06-29 15:35:09 +0700

    coordinatorLayout

---
+++ app/src/main/java/com/example/rufflez/myapplication/adapter/RecyclerViewDataAdapter.java
@@ -0,0 +1,102 @@
+package com.example.rufflez.myapplication.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rufflez.myapplication.R;
+import com.example.rufflez.myapplication.model.SectionDataModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HoangLong on 6/27/2017.
+ */
+
+public class RecyclerViewDataAdapter extends RecyclerView.Adapter<RecyclerViewDataAdapter.ItemRowHolder> {
+
+    private ArrayList<SectionDataModel> dataList;
+    private Context mContext;
+
+    public RecyclerViewDataAdapter(Context context, ArrayList<SectionDataModel> dataList) {
+        this.dataList = dataList;
+        this.mContext = context;
+    }
+
+    @Override
+    public ItemRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, null);
+        ItemRowHolder mh = new ItemRowHolder(v);
+        return mh;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemRowHolder itemRowHolder, int i) {
+
+        final String sectionName = dataList.get(i).getHeaderTitle();
+
+        ArrayList singleSectionItems = dataList.get(i).getAllItemsInSection();
+
+        itemRowHolder.itemTitle.setText(sectionName);
+
+        SectionListDataAdapter itemListDataAdapter = new SectionListDataAdapter(mContext, singleSectionItems);
+
+        itemRowHolder.recycler_view_list.setHasFixedSize(true);
+        itemRowHolder.recycler_view_list.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.HORIZONTAL, false));
+        itemRowHolder.recycler_view_list.setAdapter(itemListDataAdapter);
+
+
+        itemRowHolder.btnMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Toast.makeText(v.getContext(), "click event on more, "+sectionName , Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+
+       /* Glide.with(mContext)
+                .load(feedItem.getImageURL())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .error(R.drawable.bg)
+                .into(feedListRowHolder.thumbView);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != dataList ? dataList.size() : 0);
+    }
+
+    public class ItemRowHolder extends RecyclerView.ViewHolder {
+
+        protected TextView itemTitle;
+
+        protected RecyclerView recycler_view_list;
+
+        protected TextView btnMore;
+
+
+
+        public ItemRowHolder(View view) {
+            super(view);
+
+            this.itemTitle = (TextView) view.findViewById(R.id.itemTitle);
+            this.recycler_view_list = (RecyclerView) view.findViewById(R.id.recycler_view_list);
+            this.btnMore= (TextView) view.findViewById(R.id.btnMore);
+
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/rufflez/myapplication/fragments/HomeFragment.java
@@ -0,0 +1,62 @@
+package com.example.rufflez.myapplication.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rufflez.myapplication.R;
+import com.example.rufflez.myapplication.adapter.RecyclerViewDataAdapter;
+import com.example.rufflez.myapplication.model.SectionDataModel;
+import com.example.rufflez.myapplication.model.SingleItemModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rufflez on 6/20/15.
+ */
+public class HomeFragment extends Fragment{
+    ArrayList<SectionDataModel> allSampleData = new ArrayList<SectionDataModel>();;
+    RecyclerView mRecycleView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.floating_labels, container, false);
+        mRecycleView = (RecyclerView) rootView.findViewById(R.id.my_recycler_view);
+        return rootView;
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        // recyclerview
+        LinearLayoutManager layoutManager = new LinearLayoutManager(
+                getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecycleView.setLayoutManager(layoutManager);
+        createDummyData();
+        RecyclerViewDataAdapter adapter = new RecyclerViewDataAdapter(getActivity(), allSampleData);
+
+        mRecycleView.setAdapter(adapter);
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public void createDummyData() {
+        for (int i = 1; i <= 1; i++) {
+
+            SectionDataModel dm = new SectionDataModel();
+
+            dm.setHeaderTitle("Section " + i);
+
+            ArrayList<SingleItemModel> singleItem = new ArrayList<SingleItemModel>();
+            for (int j = 0; j <= 1; j++) {
+                singleItem.add(new SingleItemModel("Item " + j, "URL " + j));
+            }
+            dm.setAllItemsInSection(singleItem);
+
+            allSampleData.add(dm);
+
+        }
+    }
+}
